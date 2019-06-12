779adf411278a80184104c8a0d8ff61c858dfc3d
Amanda Phillipson <50961133+amandaphillipson@users.noreply.github.com>
2019-06-12 08:48:36 -0400

    Add files via upload

---
+++ w3d2-money-amandaphillipson/money.py
@@ -0,0 +1,113 @@
+class DifferentCurrencyError(Exception):
+
+    # except DifferentCurrencyError as e:
+    except DifferentCurrencyError as:
+        print("Error: Cannot add or subtract using a different currency.")
+        # print(type(e))
+        # print(e)
+        # pass
+
+class Currency:
+    """
+    Represents a currency. Does not contain any exchange rate info.
+    """
+
+    def __init__(self, name, code, symbol=None, digits=2):
+        """
+        Parameters:
+        - name -- the English name of the currency
+        - code -- the ISO 4217 three-letter code for the currency
+        - symbol - optional symbol used to designate currency
+        - digits -- number of significant digits used
+        """
+        self.name = name
+        self.code = code 
+        self.symbol = symbol
+        self.digits = digits
+
+    def __str__(self):
+        """
+        Should return the currency code, or code with symbol in parentheses.
+        """
+        return self.code
+
+    def __eq__(self, other):
+        """
+        All fields must be equal to for the objects to be equal.
+        """
+        return (type(self) == type(other) and self.name == other.name and
+                self.code == other.code and self.symbol == other.symbol and
+                self.digits == other.digits)
+
+
+class Money:
+    """
+    Represents an amount of money. Requires an amount and a currency.
+    """
+
+    def __init__(self, amount, currency):
+        """
+        Parameters:
+        - amount -- quantity of currency
+        - currency -- type of currency
+        """
+        self.amount = amount
+        self.currency = currency
+
+    def __str__(self):
+        """
+        Should use the currency symbol if available, else use the code.
+        Use the currency digits to determine number of digits to show.
+        """
+        if self.currency.symbol is not None:
+            return f"{self.currency.symbol}{self.amount:.{self.currency.digits}f}"
+        else:
+            return f"{self.currency.code} {self.amount:.{self.currency.digits}f}"
+
+    def __repr__(self):
+        return f"<Money {str(self)}>"
+
+    def __eq__(self, other):
+        """
+        All fields must be equal to for the objects to be equal.
+        """
+        return (type(self) == type(other) and self.amount == other.amount and
+                self.currency == other.currency)
+
+    def add(self, other):
+        """
+        Add two money objects of the same currency. If they have different
+        currencies, raise a DifferentCurrencyError.
+        """
+        if self.currency == other.currency:
+            return Money(self.amount + other.amount, self.currency)
+        else:
+            raise DifferentCurrencyError
+
+    def sub(self, other):
+        """
+        Subtract two money objects of the same currency. If they have different
+        currencies, raise a DifferentCurrencyError.
+        """
+        if self.currency == other.currency:
+            return Money(self.amount - other.amount, self.currency)
+        else:
+            raise DifferentCurrencyError
+
+    def mul(self, multiplier):
+        """
+        Multiply a money object by a number to get a new money object.
+        """
+        return Money(self.amount * multiplier, self.currency)
+
+    def div(self, divisor):
+        """
+        Divide a money object by a number to get a new money object.
+        """
+        return Money(self.amount / divisor, self.currency)
+
+
+# NOTES:
+#   object.method (self.currency)
+#   __init__ (initiate new class)
+#   Classes are named with "camel case" - Capitol lettser and no punctuaion/space inbetween.
